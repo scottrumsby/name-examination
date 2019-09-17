86d9c9949a040a9b7e6a470b2a0adb260e64e5c5
Scott Rumsby <scott@olivewood.io>
2019-09-17 15:28:03 -0700

    removed bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 import Vuelidate from 'vuelidate'
 import Vuetify from 'vuetify'
 import VDragged from 'v-dragged'
@@ -15,7 +14,6 @@ import store from '@/store'
 import 'vuetify/dist/vuetify.min.css'
 import '../static/stylus/overrides.styl'
 
-Vue.use(BootstrapVue)
 Vue.use(Plugin)
 Vue.use(require('vue-shortkey'))
 Vue.use(Vuelidate)
